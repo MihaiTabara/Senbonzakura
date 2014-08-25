3a44ca5a9dbe36124af51d12282fe4a5061e987c
Mihai Tabara <tabara.mihai@gmail.com>
2014-08-25 16:52:20 -0700

    Quick fixings in docker_init.sh

--- docker_init.sh
+++ docker_init.sh
@@ -1,11 +1,8 @@
 set -e
-echo "Starting RabbitMQ"
-echo "Already started right after installation."
+echo "Skip starting RabbitMQ - already started right after installation."
 
-#rabbitmq-server -detached
-#echo AMQ pid: $!
 echo "Starting celery as a daemon"
 celery worker --detach -f /var/log/celerylog.log -l DEBUG -A senbonzakura.backend.tasks
-#celery worker --detach -f /usr/local/var/log/celerylog.log -l DEBUG -A senbonzakura.backend.tasks
+
 echo "Starting flask"
-python senbonzakura/frontend/api.py
+python senbonzakura/frontend/api.py &
